9d38c7f05aab1518b680431ab4fa49d199e17f1c
John Balke <john.balke@gmail.com>
2021-06-25 17:11:46 +0100

    Add to list on Enter

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -10,11 +10,18 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState('');
   const inputRef = useFocus();
 
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      onAdd(text);
+    }
+  };
+
   return (
     <NewItemFormContainer>
       <NewItemInput
         value={text}
         onChange={(e) => setText(e.target.value)}
+        onKeyPress={handleAddText}
         ref={inputRef}
       />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
